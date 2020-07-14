402196f2fd76032afce567194e379b1e41fdee8b
Maruti Agarwal <maruti.agarwal@meltwater.com>
2020-07-14 13:21:42 -0400

    added few comments

---
+++ {{ cookiecutter.repo_name }}/src/main.py
@@ -0,0 +1,6 @@
+import sys
+sys.path.append('../src/')
+
+'''
+    This file is where all the modules (data/features/models/visualization) come together.
+'''
--- {{ cookiecutter.repo_name }}/src/samplefunction.py
+++ {{ cookiecutter.repo_name }}/src/samplefunction.py
@@ -1,3 +1,7 @@
+'''
+This is a sample file to demonstrate Unittesting in python
+Delete this file and its corresponding test script in the tests/ folder when you create a new project.
+'''
 class SampleFunction(object):
     def __init__(self):
         self.state = 0
