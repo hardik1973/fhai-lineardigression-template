a8cf42e1a03b92560409f68aa0f42f38fffc1e21
Maruti Agarwal <maruti.agarwal@meltwater.com>
2020-07-14 11:30:36 -0400

    added a sample unittest

---
+++ {{ cookiecutter.repo_name }}/src/samplefunction.py
@@ -0,0 +1,9 @@
+class SampleFunction(object):
+    def __init__(self):
+        self.state = 0
+ 
+    def increment_state(self): 
+        self.state += 1
+ 
+    def clear_state(self): 
+        self.state = 0
---
+++ {{ cookiecutter.repo_name }}/tests/test_samplefunction.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append('../src/')
+import unittest
+
+from src.samplefunction import SampleFunction
+
+
+class TestSampleFunction(unittest.TestCase):
+    def setUp(self):
+        self.func = SampleFunction()
+
+    def test_1(self):
+        self.assertTrue(True)
+
+    def test_2(self):
+        self.assertTrue(True)
+
+    def test_3(self):
+        self.assertEqual(self.func.state, 0)
+
+    def test_4(self):
+        self.func.increment_state()
+        self.assertEqual(self.func.state, 1)
+
+    def test_5(self):
+        self.func.increment_state()
+        self.func.increment_state()
+        self.func.clear_state()
+        self.assertEqual(self.func.state, 0)
+
+
+if __name__ == '__main__':
+    unittest.main()
