37be5cf327a58224fa875e222094f54209d35258
Maruti Agarwal <maruti.agarwal@meltwater.com>
2020-08-13 15:11:01 -0400

    minor changes

--- {{ cookiecutter.repo_name }}/src/lib/decorators.py
+++ {{ cookiecutter.repo_name }}/src/lib/decorators.py
@@ -14,10 +14,10 @@ def timer(func):
     """
     @functools.wraps(func)
     def wrapper_timer(*args, **kwargs):
-        start_time = time.perf_counter()    # 1
+        start_time = time.perf_counter()
         value = func(*args, **kwargs)
-        end_time = time.perf_counter()      # 2
-        run_time = end_time - start_time    # 3
+        end_time = time.perf_counter()
+        run_time = end_time - start_time
         print(f"Finished {func.__name__!r} in {run_time:.4f} secs")
         return value
     return wrapper_timer
@@ -29,12 +29,12 @@ def debug(func):
     """
     @functools.wraps(func)
     def wrapper_debug(*args, **kwargs):
-        args_repr = [repr(a) for a in args]                      # 1
-        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]  # 2
-        signature = ", ".join(args_repr + kwargs_repr)           # 3
+        args_repr = [repr(a) for a in args]
+        kwargs_repr = [f"{k}={v!r}" for k, v in kwargs.items()]
+        signature = ", ".join(args_repr + kwargs_repr)
         print(f"Calling {func.__name__}({signature})")
         value = func(*args, **kwargs)
-        print(f"{func.__name__!r} returned {value!r}")           # 4
+        print(f"{func.__name__!r} returned {value!r}")
         return value
     return wrapper_debug
 
@@ -86,7 +86,7 @@ try:
 
 except ImportError:
     def do_profile(follow=[]):
-        "Helpful if you accidentally leave in production!"
+        """Helpful if you accidentally leave in production!"""
         def inner(func):
             def nothing(*args, **kwargs):
                 return func(*args, **kwargs)
